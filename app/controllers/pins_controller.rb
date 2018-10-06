8628cb2826de99e4b20cc1f387dbac9ad5f0d47b
rcburnup <rcburnup@gmail.com>
2018-10-06 14:46:51 -0400

    Added method in pins controller to find a pin by ID, created a view to show individual pin

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,4 +1,5 @@
 class PinsController < ApplicationController
+  before_action :find_pin, only: [:show, :edit, :update, :destroy]
 
   def index
 
@@ -10,12 +11,28 @@ class PinsController < ApplicationController
 
   def create
     @pin = Pin.new(pin_params)
+
+    if @pin.save
+      redirect_to @pin, notice: "Created a new pin!"
+    else
+      render 'new'
+    end
   end
+
+  def show
+
+  end
+
 
   private
 
     def pin_params
       params.require(:pin).permit(:title, :description)
     end
+
+    def find_pin
+      @pin = Pin.find(params[:id])
+    end
+
 
 end
