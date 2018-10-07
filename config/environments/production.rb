0d7449fcf17bc35f0e620a32731781c6344254a5
rcburnup <rcburnup@gmail.com>
2018-10-07 10:54:19 -0400

    Fixed an indentation issue that was messing up Paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,15 +2,15 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Paperclip config for aws s3
-config.paperclip.defaults = {
-  storage: s3,
-  s3_credentials: {
-    bucket: ENV.fetch('S3_BUCKET_NAME'),
-    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    sc_region: ENV.fetch('AWS_REGION')
+  config.paperclip.defaults = {
+    storage: s3,
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      sc_region: ENV.fetch('AWS_REGION')
+    }
   }
-}
 
   # Code is not reloaded between requests.
   config.cache_classes = true
